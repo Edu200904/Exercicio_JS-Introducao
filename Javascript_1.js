6c5b9c784adb3ab087b982d5e2acf173d1dfeadb
Eduardo Azevedo <60852844+Edu200904@users.noreply.github.com>
2021-05-28 16:49:33 -0300

    Adicionando os arquivos da atividade.
    Os arquivos da atividade foram upados ao repositório do GitHub.

---
+++ Javascript_1.js
@@ -0,0 +1,19 @@
+function click_here () {
+    var message= "Há, em síntese, dois elementos interligados na criação e visualização de uma página web. O Cliente, um elemento do tipo ativo, encontra-se constantemente solicitando recursos e realizando requisições ao servidor. O Servidor, por sua vez, um elemento reativo, somente revela sua presença mediante à reações de ações externas. O conjunto de arquivos DHTML (Dynamic HTML), ou seja, HTML, CSS e JavaScript, são executados no modo cliente; porém, são essencialmente compilados na parte do servidor."
+    alert(message);
+}
+
+function button1 () {
+    var history= "A linguagem de programação JavaScript foi criada em 1995 pela empresa NetScape. Na época, era responsável pela validação de formulários no lado cliente, sem a necessidade de se enviar múltiplas requisições ao servidor, rodando na máquina onde uma página estava sendo visualizada pelo navegador de um indivíduo qualquer. Além disso, possibilitou uma maior interação entre os usuários e as páginas web, alterando seu comportamento e conferindo-as maior dinamismo, visto que antigamente eram completamente estáticas. É necessário destacar que JavaScript NÃO POSSUI RELAÇÃO NENHUMA com a linguagem Java, apenas a sintaxe de ambas que é semelhante. É, portanto, uma linguagem interpretada em tempo de execução. Outro ponto interessante é que JavaScript é dinamicamente tipada, isto é, não é necessário a determinação e definição do tipo de variáveis, pois elas podem ser alteradas dinamicamente no tempo de execução; basta, desse modo, colocar |var|."
+    alert(history);
+}
+
+function paragraph () {
+    var characteristics="A linguagem JavaScript é Case Sensitive, ou seja, sensível às diferenças no tipo de fonte (a utilização de letras maiúsculas e minúsculas provoca diferenças no código, já que são consideradas como variáveis distintas). A extensão do arquivo JavaScript é |js.|. Tal arquivo pode ser inserido no meio do código HTML (não recomendado), ou na head do mesmo por meio de uma referência de scripts (recomendado). O JavaScript possui, em síntese, três componentes básicos: variáveis  (armazenam dados e definem valores), funções (blocos de códigos, definem comportamentos e ações para uma página) e eventos (funções específicas relacionadas com a interação entre o usuário e a página). Existem três tipos de variáveis: |var| (tipo básico de variável, podendo ser alterada dinamicamente durante a execução), |let| (variável que funciona somente dentro do bloco ao qual foi definida) e |const| (variável constante, deve ser criada já com um valor pré-determinado e o mesmo não pode ser modificado posteriormente)."
+    alert(characteristics);
+}
+
+function example () {
+    var end="Um exemplo de comando JavaScript é o |alert|, que faz justamente o que você tem visualizado nessa simples página agora: abrir pequenas janelas de texto contendo mensagens definidas pelo programador."
+    alert(end);
+}
